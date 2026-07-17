64afdaea7eb4c04b56113f7479555feb4129b188
luisacochran-cmyk <luisa.cochran@gmail.com>
2026-07-17 20:04:55 +0200

    feat(home): add locations section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,19 @@ import {
   ConditionsSection,
   SubtitleSection,
   DescriptionSection,
+  LocationsSection,
   AdvancedTherapySection,
 } from "@/components/client-animated-section"
-
 export default function Home() {
   return (
     <>
-      <HeroSection />
-      <ConditionsSection />
-      <TreatmentsSection />
-      <SubtitleSection />
-      <DescriptionSection />
-      <AdvancedTherapySection />
+     <HeroSection />
+<ConditionsSection />
+<TreatmentsSection />
+<SubtitleSection />
+<DescriptionSection />
+<LocationsSection />
+<AdvancedTherapySection />
     </>
   )
 }
