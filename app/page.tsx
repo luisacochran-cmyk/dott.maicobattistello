e9a1cfea1e7ae211be2a44b173e579fd52dd78bf
luisacochran-cmyk <luisa.cochran@gmail.com>
2026-08-21 15:23:04 +0200

    Remove remaining LocationsSection reference

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <TreatmentsSection />
       <SubtitleSection />
       <DescriptionSection />
-      <LocationsSection />
       <FaqSection />
       <AdvancedTherapySection />
     </>
