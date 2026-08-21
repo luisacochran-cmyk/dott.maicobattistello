55f067af6d60709235bd8beb11310b41e58bf0ba
luisacochran-cmyk <luisa.cochran@gmail.com>
2026-08-21 16:27:54 +0200

    Show doctor branding on mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -81,11 +81,11 @@ export default function Navbar({ onMenuToggle = () => {} }: NavbarProps) {
       quality={100}
     />
 
-    <div className="hidden sm:flex flex-col leading-tight">
-      <span className="text-white text-xl font-bold whitespace-nowrap">
+    <div className="flex flex-col leading-tight">
+     <span className="text-white text-base sm:text-xl font-bold whitespace-nowrap">
         Dott. Maico Battistello
       </span>
-      <span className="text-white/90 text-xs font-medium whitespace-nowrap">
+      <span className="text-white/90 text-[9px] sm:text-xs font-medium whitespace-nowrap">
         Medico Legale | Osteopata | Ozonoterapeuta
       </span>
     </div>
